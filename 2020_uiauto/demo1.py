0dc20cec8113a2c128d219c3072d48383d3f315a
linOnePlus <872690700@qq.com>
2020-05-02 23:41:03 +0800

    2020.5.2

---
+++ 2020_uiauto/demo1.py
@@ -0,0 +1,26 @@
+# This sample code uses the Appium python client
+# pip install Appium-Python-Client
+# Then you can paste this into a file and simply run with Python
+# 这个是appium desktop自动生成的代码
+from appium import webdriver
+
+caps = {}
+caps["platformName"] = "Android"
+caps["deviceName"] = "lin"
+caps["appPackage"] = "com.xueqiu.android"
+caps["appActivity"] = ".view.WelcomeActivityAlias"
+caps["autoGrantPermissions"] = "true"
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+driver.implicitly_wait(10) #增加隐式等待
+
+el1 = driver.find_element_by_id("com.xueqiu.android:id/tv_agree")
+el1.click()
+el2 = driver.find_element_by_id("com.xueqiu.android:id/tv_search")
+el2.click()
+el3 = driver.find_element_by_id("com.xueqiu.android:id/search_input_text")
+el3.send_keys("google")
+el4 = driver.find_element_by_id("com.xueqiu.android:id/stockName")
+el4.click()
+
+driver.quit()
